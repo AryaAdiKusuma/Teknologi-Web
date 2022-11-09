fa8b34a3d3804612d025796a177eb43cf178b12b
AryaAdiKusuma <115007708+AryaAdiKusuma@users.noreply.github.com>
2022-11-09 12:56:55 +0800

    Add files via upload

---
+++ UTS/frontpage/landingpage.blade.php
@@ -0,0 +1,110 @@
+<x-home-layout class>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Puskesmas Banjar 2</title>
+    <link rel="stylesheet" href="../../dist/dokter/dokter.css">
+</head>
+<body>
+    <header class=" text-white ">
+        <nav class="bg-blue-800 p-2  sticky top-0 overflow-auto ">
+            <div class=" ml-3 ">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/4/45/Lambang_Kabupaten_Buleleng.png" alt="logo" class=" float-left h-28">
+                <h1 class="float-left font-semibold text-xl block mt-5 ml-6">Gobleg Water Fall
+                    <span class="block">Kabupaten Buleleng </span>
+                    <span class="block"> Provinsi Bali</span>
+                </h1>
+            </div>
+        
+           <ul class=" p-4 overflow-hidden text-right ">
+               <li class="inline list-none mr-5 w-3"><a href="hospital.html" class="text-base font-semibold  mt-7 uppercase transition-all hover:underline hover:bg-blue-600">Home</a></li>
+               <li class="inline list-none mr-5 w-3"><a href="#" class="text-base font-semibold  mt-7 uppercase transition-all hover:underline hover:bg-blue-600">Profil</a></li>
+               <li class="inline list-none mr-5 w-3"><a href="#" class="text-base font-semibold  mt-7 uppercase transition-all hover:underline hover:bg-blue-600">Layanan dan Fasilitas</a></li>
+               <li class="inline list-none mr-5 w-3"><a href="#" class="text-base font-semibold  mt-7 uppercase transition-all hover:underline hover:bg-blue-600">Informasi </a></li>
+               <li class="inline list-none mr-5 w-3"><a href="#" class="text-base font-semibold  mt-7 uppercase transition-all hover:underline hover:bg-blue-600">Daftar</a></li>
+           </ul>
+       </nav>
+    </header>
+    <main class="">
+        <div>          
+            <div class="flex items-center justify-center content-center p-3">
+                <div class="space-y-2 py-3">
+                    <div class="flex gap-3 flex-row max-md:flex-col">
+                       
+                        <div class="max-w-sm rounded-md overflow-hidden shadow-slate-900 bg-blue-800  hover:scale-105 transition-transform ">
+                            <img class="w-full " src="https://cdn-2.tstatic.net/makassar/foto/bank/images/pengurus-genpi-barru-family-trip-di-embung-paccekke-dan-air-terjun-manuba-barru.jpg">
+                            <div class="px-6 py-4">
+                            <div class="font-bold text-xl mb-2 text-white text-center">Reguler</div>
+                            <p class="text-gray-100 text-base">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                            </p>
+                            </div>
+                            <div class="py-5 flex items-center justify-center">
+                                <a href="#">
+                                <button id="tiket" class=" bg-blue-600 w-52 text-white font-semibold py-2 rounded-full hover:bg-blue-400 hover:underline transition-colors">Buy Now</button>
+                                </a>
+                            </div> 
+                        </div>
+                        <div class="max-w-sm rounded-md overflow-hidden shadow-2xl bg-blue-800  hover:scale-105 transition-transform">
+                            <img class="w-full" src="https://get.pxhere.com/photo/water-rock-waterfall-person-standing-rapid-fjord-body-of-water-wasserfall-water-feature-1409683.jpg">
+                            <div class="px-6 py-4">
+                            <div class="font-bold text-xl mb-2 text-white text-center">Family</div>
+                            <p class="text-gray-100 text-base">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                            </p>
+                            </div>
+                            <div class="py-5 flex items-center justify-center">
+                                <a href="#">
+                                <button id="tiket" class=" bg-blue-600 w-52 text-white font-semibold py-2 rounded-full hover:bg-blue-400 hover:underline transition-colors">Buy Now</button>
+                                </a>
+                            </div> 
+                        </div>
+                        <div class="max-w-sm rounded-md overflow-hidden shadow-2xl bg-blue-800  hover:scale-105 transition-transform">
+                            <img class="w-full" src="https://img.antaranews.com/cache/730x487/2019/08/26/IMG-20190825-WA0029_1.jpg.webp">
+                            <div class="px-6 py-4">
+                            <div class="font-bold text-xl mb-2 text-white text-center">Family and Hotel</div>
+                            <p class="text-gray-100 text-base">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                            </p>
+                            </div>
+                            <div class="py-5 flex items-center justify-center">
+                                <a href="#">
+                                <button id="tiket" class=" bg-blue-600 w-52 text-white font-semibold py-2 rounded-full hover:bg-blue-400 hover:underline transition-colors">Buy Now</button>
+                                </a>
+                            </div> 
+                        </div>
+                        <div class="flex justify-center">
+                            <div class="max-w-sm rounded-md overflow-hidden shadow-lg bg-white">
+                                <div class="px-6 py-4">
+                                <div class="font-bold text-xl mb-2 text-black text-center">SHOPPING CART</div>
+                                
+                                <div class="flex justify-start">
+                                <p class="text-gray-700 text-base text-center">
+                                    Your Purchase:
+                                </p>
+                                </div>
+                                <div class="flex justify-start">
+                                <p class="text-gray-700 text-lg text-center font-bold">
+                                    Total Purchase:
+                                </p>
+                                </div>
+                                <div class="py-5 flex items-center justify-center">
+                                    <a href="./ticketPage.html">
+                                        <button id="tiket" class="bg-blue-600 w-52 text-white font-semibold py-2 rounded-full hover:bg-blue-400 transition-colors">Checkout</button>
+                                    </a>
+                                </div>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div> 
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
+</x-home-layout>
